ee0c50336427168adc757cb0c169b70f03614130
tonniegeetz <tonniegeetz@gmail.com>
2024-04-17 22:23:44 +0300

    Done the deliverables for the miniproject

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
-document.addEventListener("DOMContentLoaded", () => {
+document.addEventListener('DOMContentLoaded', () => {
   // your code here
+  const form = document.querySelector('form');
+  form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    e.target.task.value;
+    buildTask(e.target.task.value);
+  });
+
+  function buildTask(task) {
+    let li = document.createElement('li');
+    li.textContent = task;
+    console.log(li);
+    document.querySelector('#tasks').appendChild(li);
+  }
 });
